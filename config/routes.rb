43bba7d4d0a7bf6ea546794ad76d3c78ced9d5ae
hidakashun <hidakashun60@gmail.com>
2023-11-23 09:05:42 +0000

    [add]resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:new, :index, :show, :edit]
 end
