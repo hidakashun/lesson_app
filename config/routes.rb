976cc2378e620c32e2406f012a966926d8a67cad
hidakashun <hidakashun60@gmail.com>
2023-11-23 08:54:27 +0000

    [add]homesコントローラー、ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'homes#top'
   get 'lists/new'
-  root 'lists/index'
+  get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
