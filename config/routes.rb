bb2602acdebf3c905f58b1b1be796ee17938516d
hidakashun <hidakashun60@gmail.com>
2023-11-23 08:47:49 +0000

    [add]ルートパス

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
+  root 'lists/index'
   get 'lists/show'
   get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
