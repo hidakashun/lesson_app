0c14d02281eb0b47e9124a2b8bb1cb951e26afee
hidakashun <hidakashun60@gmail.com>
2023-11-23 10:02:18 +0000

    [add]投稿機能

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,16 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create# 保存機能を追加
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. 一時的にトップ画面へリダイレクト
+    redirect_to root_path
   end
 
   def index
@@ -9,5 +20,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params# 保存機能を追加
+    params.require(:list).permit(:title, :body)
   end
 end
