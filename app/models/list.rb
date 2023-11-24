2a0df71bec4c64b33e5f9430ab456a3bac54617e
hidakashun <hidakashun60@gmail.com>
2023-11-24 13:03:04 +0000

    [add]画像投稿枚数バリデーション

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,15 @@
 class List < ApplicationRecord
+
   has_many_attached :list_images
+
+  FILE_NUMBER_LIMIT = 3
+
+  validate :validate_number_of_files
+
+  private
+
+  def validate_number_of_files
+  return if list_images.length <= FILE_NUMBER_LIMIT
+  errors.add(:list_images,"に添付できる画像は#{FILE_NUMBER_LIMIT}件までです。")
+  end
 end
